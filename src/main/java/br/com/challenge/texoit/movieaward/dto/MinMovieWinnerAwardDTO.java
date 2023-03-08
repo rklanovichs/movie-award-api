ef9d013f8620c6c727fe166c5b279271fa5de798
Robson Klanovichs <robson.klanovichs@terceiros.calcard.com.br>
2023-03-08 01:42:27 -0300

    feat: Versão inicial desafio técnico TEXO IT

---
+++ src/main/java/br/com/challenge/texoit/movieaward/dto/MinMovieWinnerAwardDTO.java
@@ -0,0 +1,16 @@
+package br.com.challenge.texoit.movieaward.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MinMovieWinnerAwardDTO {
+    String producer;
+    Integer interval;
+    Integer previousWin;
+    Integer followingWin;
+
+}
